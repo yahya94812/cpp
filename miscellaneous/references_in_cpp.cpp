307f0ee19c4223282ba590134dabc543a1165647
yahya94812 <yahya94812@gmail.com>
2025-05-22 21:54:33 +0530

    references in cpp completed

--- miscellaneous/references_in_cpp.cpp
+++ miscellaneous/references_in_cpp.cpp
@@ -1,4 +1,4 @@
-// In C++, references are essentially aliases for existing variables. They allow you to access and modify the original variable using a different name.
+// In C++, references are essentially aliases for existing variables. They allow you to access and modify the original variable using a different name. 
 // A reference must be initialized when it's declared.
 // Once set, a reference cannot be changed to refer to a different variable.
 // A reference is not a copy of the variable; it is another name for the same memory location.
@@ -6,37 +6,33 @@
 #include <iostream>
 using namespace std;
 
-void update(int &);
+void update(int&);
 
-int main()
-{
+int main(){
     int x = 10;
-    int &ref = x;
+    int& ref = x;
     ref = 20;
 
-    cout << x << endl;
+    cout<<x<<endl;
 
     update(x);
 
-    cout << x << endl;
+    cout<<x<<endl;
 
     return 0;
 }
 
-void update(int &val)
-{
-    // it avoid copying and allow modification
+void update(int& val){ 
+    //it avoid copying and allow modification
     val += 5;
 }
 
-int &get_element(int arr[], int index)
-{
+int& get_element(int arr[], int index){
     //(to return an actual variable, not a copy
     return arr[index];
 }
 
-void print(const int &val)
-{
-    // To refer to something without modifying it
+void print(const int& val){
+    //To refer to something without modifying it
     cout << val;
 }
